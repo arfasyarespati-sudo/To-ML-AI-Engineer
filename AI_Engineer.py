e064485ed8760a8edc45b0cc7591024f32894d3c
Muhammad Arfasya <arfasyarespati@gmail.com>
2025-10-17 13:15:37 +0700

    AI & ML

--- AI_Engineer.py
+++ AI_Engineer.py
@@ -8,11 +8,12 @@ class GPU:
 GraphicsCard1 = GPU()
 GraphicsCard2 = GPU()
 
-GraphicsCard2.driver = "RADEON RX Series"
+GraphicsCard2.driver = "RADEON RX SERIES"
 
 print(GraphicsCard1.driver)
 print(GraphicsCard2.driver)
 print()
+
 class Mobil:
     def __init__(self, warna, merek, kecepatan):
         self.warna = warna
@@ -22,4 +23,14 @@ mobil_1 = Mobil("Merah", "Toyota", 120)
 print(mobil_1.warna)
 print(mobil_1.merek)
 print(mobil_1.kecepatan)
+print()
+class MobilSport(Mobil):
+    def turbo(self):
+        self.kecepatan += 50
+
+MobilSport_1 = MobilSport("Merah", "Buggati", 160)
+MobilSport_1.turbo()
+print(MobilSport_1.kecepatan)
+
+
 
