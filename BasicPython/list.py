2e25742481e94131278db648cbf964c7d06d19bf
Muhammad Arfasya <arfasyarespati@gmail.com>
2025-12-24 17:21:35 +0700

    Python Fundamentals

--- BasicPython/list.py
+++ BasicPython/list.py
@@ -1,6 +1,30 @@
 #LIST PY
 print("---- LIST ----")
-mylist = ["Apel", "Mangga", "Jeruk"]
-print(mylist)
-print(mylist[0])
-print(len(mylist))
+mylist1 = ["Apel", "Mangga", "Jeruk", "Markisa", "Nangka"]
+mylist2 = ["Mobil", 12, True]
+print(mylist1)
+print(mylist1[0])
+print(len(mylist1))
+print(type(mylist2))
+print("----")
+print(mylist1[-3:-1])
+#Tambahkan item urutan berikutnya
+mylist1.append("Jambu")
+print(mylist1)
+#Masukkan item di index
+mylist1.insert(1, "Durian")
+print(mylist1)
+#Loopers
+print("--- Loopers ---")
+
+for x in range (len(mylist1) - 3):
+    print(mylist1[x])
+print("----")
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+
